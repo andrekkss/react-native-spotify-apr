5ea87ee12c414de825b4d9747bbfed48dce017bb
Andre <andre.kitano.silva@gmail.com>
2020-12-31 05:22:26 -0300

    Updated spotify object to use env and refactoring methods for connection to Spotify SDK

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,61 +1,45 @@
 import * as React from 'react';
 
 import { StyleSheet, View} from 'react-native';
-import type Spotify from 'react-native-spotify-apr';
-import APR from 'react-native-spotify-apr';
+import Spotify from 'react-native-spotify-apr';
 import type IPlaylist from 'src/spotify/model/spotify_playlist';
 import { Card, Title, Paragraph } from 'react-native-paper';
 import { useEffect } from 'react';
 
-const MCard = (props) => (
-  <Card onPress={() => {props.callback(props.id)}}>
-    <Card.Cover source={{ uri: props.image }} />
-    <Card.Content>
-      <Title>{props.title}</Title>
-      <Paragraph>by: {props.by}</Paragraph>
-    </Card.Content>
-  </Card>
-);
-
 export default function App() {
-  const [spotify, setSpotify] = React.useState<Spotify | undefined>();
-  const [token, setToken] = React.useState<string | undefined>();
+  const [spotify] = React.useState<typeof Spotify>(Spotify);
   const [playlists, setPlaylists] = React.useState<IPlaylist[]>();
 
-  const clientID = "459ca320939b4fa28d2fda69ea23e0fa"
-  const clientCallbackUrl = "http://com.example.reactnativespotifyapr/callback"
-
   useEffect(() => {
-    async function connect() {
-      const spotify = new APR(clientID, clientCallbackUrl);
-      spotify.connect((message: string) => console.log(message));
-      await spotify?.auth()
-      setSpotify(spotify);
-      spotify?.getToken().then((value: string) => {
-        console.log(token)
-        setToken(value)
-      })
-      spotify?.getPlaylist().then((playlists: IPlaylist[]) => {
-        setPlaylists(playlists);
-      })
+    function connect() {
+      spotify
+        .connect((message: string) => console.log(message))
+        .auth()
+        .then((_: string) => {
+          spotify?.getPlaylist().then((playlists: IPlaylist[]) => {
+            setPlaylists(playlists);
+          }).catch((err: any) => {
+            console.log(err);
+          })
+        })
     }
     connect()
   }, [])
 
   function play(id: string){
-    console.log(id);
-    spotify?.play(id);
+    spotify.play(id);
   }
 
   return (
     <View style={styles.container}>
-      {playlists?.map((playlist: IPlaylist) => {
-        return <MCard 
-          id={playlist.id} 
-          name={playlist.name} 
-          image={playlist.images[0].url} 
-          by={playlist.owner.display_name} 
-          callback={play} />
+      {playlists?.map((playlist: IPlaylist, _: number) => {
+        return <Card onPress={() => {play(playlist.id)}}>
+          <Card.Cover source={{ uri: playlist.images[0].url }} />
+          <Card.Content>
+            <Title>{playlist.name}</Title>
+            <Paragraph>by: {playlist.owner.display_name}</Paragraph>
+          </Card.Content>
+        </Card>
       })}
     </View>
   );
