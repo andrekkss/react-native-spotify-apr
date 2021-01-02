51e0eba44a7d79d4ec5ecfe83dc1fe21a01accaa
Andre <andre.kitano.silva@gmail.com>
2021-01-02 03:28:20 -0300

    added new methods to player music and manipulating device

---
+++ android/src/main/java/com/reactnativespotifyapr/spotify/event/VolumeEvent.kt
@@ -0,0 +1,2 @@
+package com.reactnativespotifyapr.spotify.event
+
--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,14 +1,19 @@
 import * as React from 'react';
 
-import { StyleSheet, View} from 'react-native';
 import Spotify from 'react-native-spotify-apr';
-import type IPlaylist from 'src/spotify/model/spotify_playlist';
+
+import { StyleSheet, Text, View, Button} from 'react-native';
 import { Card, Title, Paragraph } from 'react-native-paper';
 import { useEffect } from 'react';
+
+import type IPlaylist from 'src/spotify/model/spotify_playlist';
+import type IDevice from 'src/spotify/model/spotify_device';
 
 export default function App() {
   const [spotify] = React.useState<typeof Spotify>(Spotify);
   const [playlists, setPlaylists] = React.useState<IPlaylist[]>();
+  const [volume, setVolume] = React.useState<number>()
+  const [devices, setDevices] = React.useState<IDevice[]>();
 
   useEffect(() => {
     function connect() {
@@ -27,20 +32,35 @@ export default function App() {
   }, [])
 
   function play(id: string){
-    spotify.play(id);
+    spotify.play(id)
+      .setObserverVolume((value: number) => setVolume(value))
+      .getDevices().then((devices: IDevice[]) => setDevices(devices))
+  }
+
+  function switchDevice() {
+    spotify.setDevicePlay("5b40d4292a84cc4e13b941991de3b61f884bb063")
   }
 
   return (
     <View style={styles.container}>
-      {playlists?.map((playlist: IPlaylist, _: number) => {
-        return <Card onPress={() => {play(playlist.id)}}>
-          <Card.Cover source={{ uri: playlist.images[0].url }} />
+      <Text>Current volume: {volume} </Text>
+      <Text>Current device: {devices?.find((value: IDevice) => value.is_active == true)?.name}</Text>
+      <Button
+        onPress={switchDevice}
+        title="Learn More"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
+      { playlists != undefined ?
+        <Card onPress={() => {play(playlists[0].id)}}>
+          <Card.Cover source={{ uri: playlists[0].images[0].url }} />
           <Card.Content>
-            <Title>{playlist.name}</Title>
-            <Paragraph>by: {playlist.owner.display_name}</Paragraph>
+            <Title>{playlists[0].name}</Title>
+            <Paragraph>by: {playlists[0].owner.display_name}</Paragraph>
           </Card.Content>
         </Card>
-      })}
+        : <View/>
+      }
     </View>
   );
 }
